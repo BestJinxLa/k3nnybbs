d6608810d3779609682bf6ac7fb5421cce920eb1
vagrant <vagrant@homestead>
2018-04-15 09:38:49 +0000

    user can del topics

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -75,7 +75,6 @@ class TopicsController extends Controller
 	{
 		$this->authorize('destroy', $topic);
 		$topic->delete();
-
 		return redirect()->route('topics.index')->with('message', '删除成功.');
 	}
 
